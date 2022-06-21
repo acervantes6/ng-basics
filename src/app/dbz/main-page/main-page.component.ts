bc272b8d7eff69e389ba48bfbcc083b6fdd29f84
acervantes6 <armandocervantes6@gmail.com>
2022-06-21 17:26:30 -0500

    update project

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: 'Maistro',
+    poder: 1000
+  }
+
+  constructor() {}
+}
